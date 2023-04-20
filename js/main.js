e46fed92ab74ee07047bf217b8fae872ceb67d3a
guiyee <guillermodinanno@gmail.com>
2023-04-20 19:59:59 -0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 const button = document.querySelector("#button")
 const advice = document.querySelector(".advice")
 const adviceTitle = document.querySelector(".advice-title")
+const adviceNumber = document.querySelector("#number");
+
 
 function getAdvice (){
-    fetch('https://api.adviceslip.com/advice')
+    const totalAdvice = 224;
+    const randomId = Math.floor(Math.random()*totalAdvice) + 1
+    const url = `https://api.adviceslip.com/advice/${randomId}`
+
+    fetch(url)
         .then(response => response.json())
-        .then(data =>  {advice.textContent = data.slip.advice})
-        .catch(error => console.error(error)); 
+        .then(data =>  {
+            advice.innerHTML = data.slip.advice; 
+            adviceNumber.innerHTML = ` #${randomId}`})
+        .catch(error => console.error(error))
+        .then(() => console.log("Consejo cargado exitosamente."));
+       
 }
+
 button.addEventListener('click', getAdvice) ;
+
 
