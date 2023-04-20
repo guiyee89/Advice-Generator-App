b1e13bc2e2fcb92175171a65af4c96c3c4ffa184
guiyee <guillermodinanno@gmail.com>
2023-04-20 14:21:29 -0300

    html css done -  starting js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("#button")
+const advice = document.querySelector(".advice")
+const adviceTitle = document.querySelector(".advice-title")
+
+function getAdvice (){
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data =>  {advice.textContent = data.slip.advice})
+        .catch(error => console.error(error)); 
+}
+button.addEventListener('click', getAdvice);
+
